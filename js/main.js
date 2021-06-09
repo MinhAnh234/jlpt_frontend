47d44c209408e25ddae04d36817efa9c820aaf4e
my_name <my_name@example.com>
2021-06-09 12:19:01 +0900

    2.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 window.onload=function() {
+    a=parseInt(2);
+    b=parseInt(2);
+    c=parseInt(10);
+    run=true;
+    function start() {
+        this.c--;
+        if(run) {
+            if(this.c==-1){
+                this.b-=1;
+                this.c=59;
+            }
+            if(this.b==-1) {
+                this.a-=1;
+                this.b=59;
+            }
+            if(this.a-=1) {
+                this.a=0;
+               
+            }
+        console.log(a+":"+b+":"+(this.c));
+        timeout = setTimeout(function(){
+            start();
+       },1000)
+    }
+    }
+    start();
 
+    
+   
+
+    
 }
